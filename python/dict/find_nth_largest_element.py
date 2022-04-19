5905b1310f4e09eab30b7950f80e83dc7aef9432
Jerry Verghese Cheruvathoor <manueca@gmail.com>
2022-04-19 06:29:08 -0500

    Update find_nth_largest_element.py

--- python/dict/find_nth_largest_element.py
+++ python/dict/find_nth_largest_element.py
@@ -1,1 +1,7 @@
+def findNthHighest(dict_val,n):
+        nth_max_value=sorted(set(dict_val.values()),reverse=True)[-n]
+        return nth_max_value
+
+dict_val = {"a":1,"b":5,"c":3,"d":3}
+print (findNthHighest(dict_val,2))
 
